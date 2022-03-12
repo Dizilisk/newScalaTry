17dbb3a78b32e93e24ab63b9448f91ed17e03e76
Dizilisk <44327135+Dizilisk@users.noreply.github.com>
2022-03-12 19:09:29 +0300

    Week 2 start

--- src/lectures/week2oop/CaseClasses.scala
+++ src/lectures/week2oop/CaseClasses.scala
@@ -1,5 +1,12 @@
 package lectures.week2oop
 
-object CaseClasses {
+object CaseClasses extends App {
+  val student = new Student(0, "Bob")
+  println(student.name)
+  student.uni
+}
 
+class Student(id: Int, val name: String) {
+  val uni = "University"
+  println("Student class")
 }
