4ca230524bad18de887df7df673c200ecfd43007
Dizilisk <44327135+Dizilisk@users.noreply.github.com>
2022-03-19 16:24:08 +0300

    OOP theory fin

--- src/lectures/week2oop/CaseClasses.scala
+++ src/lectures/week2oop/CaseClasses.scala
@@ -1,5 +1,30 @@
 package lectures.week2oop
 
-object CaseClasses {
+import lectures.week2oop.ClassesOOP.{Instructor, course}
 
+object CaseClasses extends App {
+  case class Person(name: String, occupation: String)
+
+  val bob = new Person("Bob", "Developer")
+  val bobsDouble = new Person("Bob", "Developer")
+  val anotherBob = bob.copy()
+  val bobsTwin = bob.copy("John")
+  val alice = Person("Alice", "Engineer")
+  println(bob.name) //доступ к name без val
+  println(bob.toString)
+  println(bob == bobsDouble)
+  println(anotherBob)
+  println(bobsTwin)
+  println(alice)
+
+  case class Course(title: String, instructor: String)
+
+  object Course {
+    def apply(instructor: String): Course = Course("AdvancedScala", instructor)
+  }
+
+  val scalaCourse = new Course("Scala", "Bob")
+
+  val course = new Course("Scala", "Bob")
+  println(course)
 }
