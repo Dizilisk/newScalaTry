110023186c3abe6f8e2e77cb1d4e5a9cc87c743e
Dizilisk <44327135+Dizilisk@users.noreply.github.com>
2022-03-20 16:56:07 +0300

    OOP ex. 1 fin

--- src/exercises/LogSystem.scala
+++ src/exercises/LogSystem.scala
@@ -1,5 +1,28 @@
 package exercises
 
-object LogSystem {
+import scala.annotation.tailrec
 
+object LogSystem extends App {
+
+  class Logger(var msgNum: Int = 0) {
+
+    def info(): Logger = {
+      println("INFO New Message")
+      new Logger(msgNum + 1)
+    }
+
+    def info(n: Int): Logger = {
+      if (n < 1) this
+      else {
+        info.info(n - 1)
+      }
+    }
+
+    def print: Unit = println(msgNum)
+  }
+
+  val logger = new Logger(0)
+  logger.info.print
+  logger.info(3).print
+  logger.info.info.info.print
 }
