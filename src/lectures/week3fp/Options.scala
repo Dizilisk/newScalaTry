b9c04a613a4babcb6b91822f661df4eddbdbc136
Dizilisk <44327135+Dizilisk@users.noreply.github.com>
2022-03-24 16:22:55 +0300

    Options fin

--- src/lectures/week3fp/Options.scala
+++ src/lectures/week3fp/Options.scala
@@ -1,5 +1,22 @@
 package lectures.week3fp
 
-object Options {
+object Options extends App {
+  def unsafeMethod(): Option[String] = None
+  def safeMethod(): String = "There is a String"
+  def maybeSafeMethod(): Option[String] = Some("There is no harm")
 
+  val unsafeRed = Option(unsafeMethod())
+  val safeRes = Option(safeMethod())
+
+  println(unsafeRed)
+  println(safeRes)
+
+  val someOptions: Option[String] = Some("Success")
+  val noneOptions: Option[Int] = None
+
+  println(someOptions.isEmpty)
+  println(noneOptions.isEmpty)
+
+  val chainedResult = unsafeMethod() orElse maybeSafeMethod()
+  println(chainedResult)
 }
