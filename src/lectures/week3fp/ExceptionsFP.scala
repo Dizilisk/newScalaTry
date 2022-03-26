efeeb7b752abc7c9f0b0429f4ec21a90f1156095
Dizilisk <44327135+Dizilisk@users.noreply.github.com>
2022-03-26 14:58:14 +0300

    ExceptionsFP fin

--- src/lectures/week3fp/ExceptionsFP.scala
+++ src/lectures/week3fp/ExceptionsFP.scala
@@ -1,5 +1,22 @@
 package lectures.week3fp
 
-object ExceptionsFP {
+import scala.util.{Try, Success, Failure}
 
+object ExceptionsFP extends App {
+  def unsafeMethod(): String = throw new RuntimeException("Sorry, not your day")
+  def myMethod(): String = "My method is valid"
+  def methodWithFails(): Try[String] = Failure(new RuntimeException("Ooops...."))
+  def methodWithSucceeds(): Try[String] = Success("Everything is OK")
+
+  val potentialFailure = Try(unsafeMethod())
+  val anotherPotentialFailure = Try {
+    //Код исключения
+  }
+  val executeWithTry = Try(unsafeMethod()).orElse(Try(myMethod()))
+  val tryMethods = methodWithFails() orElse methodWithSucceeds()
+
+  println(potentialFailure)
+  println(potentialFailure.isSuccess)
+  println(executeWithTry)
+  println(tryMethods)
 }
